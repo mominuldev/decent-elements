c2d5c4fd9f7bb13f4069887cf1eecfc0733c5781
Mominul Islam <mominulfed@gmail.com>
2025-09-06 23:09:14 +0600

    main logo and menu icon updated

--- includes/admin/backend/src/components/Navbar.jsx
+++ includes/admin/backend/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
-import ProIcon from "/crown-icon.svg";
-import PromotIcon from "/mike-icon.svg";
 import Menu from "./Menu";
 
 const Navbar = () => {
 	// Get the plugin base URL from WordPress
 	const baseUrl = window.decentElements?.baseUrl || "";
 	const logoUrl = `${baseUrl}includes/admin/assets/js/decent-logo.svg`;
+	const ProIcon = `${baseUrl}includes/admin/assets/js/crown-icon.svg`;
+	const PromotIcon = `${baseUrl}includes/admin/assets/js/mike-icon.svg`;
+	// const logoUrl = `https://decentelements.com/wp-content/uploads/2024/05/decent-logo.svg`; // For testing purpose
 
 	return (
 		<div className='bg-white h-[50px] mb-4 shadow-[0px_4px_6px_-4px_rgba(23,25,31,0.10)]'>
-			<div className='container max-w-[1200px] mx-auto'>
+			<div className='max-w-[1200px] mx-auto'>
 				<div className='flex items-center justify-between gap-2 '>
 					<div className='flex items-center'>
 						<img
